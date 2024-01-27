7bfa7a6252a96f3e9b6372390191601c16d559d0
Arifurrahmanrifat29112002 <arifurrahmanrifat72@gmail.com>
2024-01-28 00:46:27 +0600

    new update

---
+++ app/Http/Controllers/Dashbord/SettingController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Dashbord;
+
+use App\Http\Controllers\Dashbord\BaseController as BaseController;
+use App\Models\Setting;
+use Illuminate\Http\Request;
+
+class SettingController extends BaseController
+{
+     /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Setting $setting)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Setting $setting)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Setting $setting)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Setting $setting)
+    {
+        //
+    }
+}
